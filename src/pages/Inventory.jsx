bf306991a47bc05c75b730e541fe4b8d8fedacf6
mike <michael@1981media.se>
2023-12-16 02:14:32 +0100

    Fixed notification on item create

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -6,9 +6,12 @@ import { ListGroup } from "flowbite-react";
 import TopAppBar from "../lib/TopAppBar";
 import BottomNavbar from "../lib/BottomNavbar";
 import Item from "../lib/Item";
+import ItemMessage from "../lib/ItemMessage";
 
 const Inventory = () => {
   const myItems = useSelector((state) => state.items.myItems);
+  const selectedItem = useSelector((state) => state.items.selectedItem);
+
   const itemsArray = useSelector((state) => state.items.itemsArray);
   const dispatch = useDispatch();
 
@@ -26,17 +29,21 @@ const Inventory = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <TopAppBar>Your Items</TopAppBar>
-      <ListGroup className="w-full lg:mt-5">
-        {myItems?.map(({ name, description, id }) => (
-          <Item
-            key={id}
-            name={name}
-            description={description}
-            id={id}
-            className="bg-white rounded-lg shadow-md p-4 mb-4"
-          />
-        ))}
-      </ListGroup>
+      <div id="inventory" className="relative">
+        <ListGroup className="w-full lg:mt-5">
+          {myItems?.map(({ name, description, id }) => (
+            <Item
+              key={id}
+              name={name}
+              description={description}
+              id={id}
+              className="bg-white rounded-lg shadow-md p-4 mb-4"
+              active={id === selectedItem}
+            />
+          ))}
+        </ListGroup>
+        <ItemMessage />
+      </div>
       <BottomNavbar />
     </div>
   );
